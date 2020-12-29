7395f5b2161c915d74b5ad881069965d0f5c0aad
Kexi Dang <dangkexi@gmail.com>
2020-12-29 14:55:07 -0600

    support deployment to gh-pages

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # react-project-starter
 New React project without using create-react-app
 
-> This repo's default branch is `main`
+- This repo's default branch is `main`
+- The deployed version is at https://kexizeroing.github.io/react-project-starter/ using Github Pages.
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,63 @@
 const path = require("path");
 const webpack = require("webpack");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: "./src/index.js",
-  mode: "development",
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        loader: "babel-loader"
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      }
+module.exports = (env, argv) => {
+  // console.log(env, argv);
+  // for dev: { WEBPACK_SERVE: true } { env: { WEBPACK_SERVE: true } }
+  // for build: { WEBPACK_BUNDLE: true } { mode: 'production', env: { WEBPACK_BUNDLE: true } }
+  return {
+    entry: "./src/index.js",
+    mode: "development",
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          loader: "babel-loader"
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"]
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        }
+      ]
+    },
+    resolve: { extensions: ["*", ".js", ".jsx"] },
+    // This config is used for having a `public/index.html` and the bundle file is served at http://localhost:3000/dist/bundle.js
+    // output: {
+    //   path: path.resolve(__dirname, "dist/"),
+    //   publicPath: "/dist/",
+    //   filename: "bundle.js"
+    // },
+    // devServer: {
+    //   contentBase: path.join(__dirname, "public/"),
+    //   port: 3000,
+    //   publicPath: "/dist/",
+    //   hotOnly: true/
+    // },
+    
+    // This config is used for `HtmlWebpackPlugin` which will generate a `index.html` serving at http://localhost:3000
+    // `/react-project-starter/bundle.js` is used when deploy to Github Pages
+    output: {
+      path: path.resolve(__dirname, "dist/"),
+      filename: "bundle.js",
+      publicPath: argv.mode === "production" ? "/react-project-starter/" : "/",
+    },
+    devServer: {
+      port: 3000,
+      publicPath: "/",
+      hotOnly: true
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new HtmlWebpackPlugin({
+        title: "React Starter",
+        favicon: "./public/assets/favicon-32x32.png"
+      })
     ]
-  },
-  resolve: { extensions: ["*", ".js", ".jsx"] },
-  output: {
-    path: path.resolve(__dirname, "dist/"),
-    publicPath: "/dist/",
-    filename: "bundle.js"
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "public/"),
-    port: 3000,
-    publicPath: "/dist/",
-    hotOnly: true
-  },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
-};
+  };
+}
